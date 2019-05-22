36fbd789f0320e6734930403f1767e7c0fa2e8d4
vagrant <raoulbia@gmail.com>
2019-05-22 15:03:45 +0100

    update

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ from zipfile import ZipFile
 import pandas as pd
 import json
 import os
+from itertools import compress
 
 app = Flask(__name__)
 
 # app.secret_key = 'your secret'
 # app.config['SESSION_TYPE'] = 'filesystem'
 
-port = int(os.environ["PORT"])
-
-zf = ZipFile('local-data/telco-customer-churn.zip')
-df = pd.read_csv(zf.open('WA_Fn-UseC_-Telco-Customer-Churn.csv'))
+# zf = ZipFile('local-data/telco-customer-churn.zip')
+# df = pd.read_csv(zf.open('WA_Fn-UseC_-Telco-Customer-Churn.csv'))
+df = pd.read_csv('local-data/telco-customer.csv')
 print(df.head())
 
 
@@ -27,23 +27,48 @@ def index():
 
 @app.route('/upgrade', methods=['POST'])
 def upgrade():
-    answer = ''
-    upgrade_url = ''
     data = json.loads(request.get_data().decode())
     # print(data)
     # cust_id = data['nlp']['entities']['customer'][0]['raw']
     cust_id = data["conversation"]["memory"]["customer"]["customer_id"]
-    row = df[df.customerID==cust_id]
-    if len(row) > 0:
-        # answer = row.Contract.values[0]
-        if row.Contract.values[0] == 'Month-to-month':
-            answer = 'Sorry, upgrades are not available for Month-to-month contracts'
-            return respond(answer)
-        else:
-            upgrade_url = 'https://n.vodafone.ie/upgrade-your-phone.html'
-            answer = "I checked your account and I'm deligthed to confirm that you are elegible for an upgrade. " \
-                     "Click on the link below to find out more!"
-            return respond(answer, upgrade_url)
+    cust_record = df[df.CustomerID==cust_id]
+
+    if len(cust_record) > 0: # customer record exists
+
+        upgrade = cust_record.Upgrade.values[0]
+        churn = cust_record.Churn.values[0]
+        ltv = cust_record.LTV.values[0]
+        # print(upgrade, churn, ltv)
+
+        offer_reduced = all([upgrade == 'yes', churn == 'yes', ltv == 'high'])
+        offer_store_price = any([
+                                all([upgrade == 'yes', churn == 'yes', ltv == 'low']),
+                                all([upgrade == 'yes', churn == 'no'])
+                                ])
+        offer_none = all([upgrade == 'no'])
+        print(offer_reduced, offer_store_price, offer_none)
+
+        offer_reduced_text = "I checked your account and I'm deligthed to confirm that you are elegible for an upgrade " \
+                             "at a REDUCED PRICE! :) " \
+                             "Click on the link below to find out more!"
+
+        offer_store_price_text = "I checked your account and I'm deligthed to confirm that you are elegible for an upgrade. " \
+                                 "Click on the link below to find out more!"
+
+        offer_none_text = "Sorry, upgrades are not available for Pay as You Go customers. Call us on "
+
+        text = [offer_reduced_text, offer_store_price_text, offer_none_text]
+        filter = [offer_reduced, offer_store_price, offer_none]
+
+        answer = list(compress(text, filter))[0]
+
+        offer = 'store_price'
+        if offer_reduced:
+            offer = 'reduced_price'
+        if offer_none:
+            offer = None
+
+        return respond(answer, offer)
 
     else:
         answer = 'Sorry, this customer ID is not valid.'
@@ -66,7 +91,7 @@ def contract_query():
         return respond(answer, validation='no')
 
 
-def respond(answer, upgrade_url=None, validation='yes'):
+def respond(answer, offer, validation='yes'):
     return jsonify(
     status=200,
     replies=[{
@@ -75,7 +100,7 @@ def respond(answer, upgrade_url=None, validation='yes'):
       'content': '%s' % (answer)
     }],
     conversation={
-      'memory': { 'url': upgrade_url, 'validation_backend': validation}
+      'memory': { 'offer_type': offer, 'validation_backend': validation}
     }
 )
 
@@ -84,7 +109,13 @@ def errors():
   print(json.loads(request.get_data().decode()))
   return jsonify(status=200)
 
-# if __name__ == "__main__":
-#     app.run(debug=True, host = '0.0.0.0', port = 5000)
 
-app.run(port=port, host="0.0.0.0")
+if __name__ == "__main__":
+    # comment/ uncomment below accordingly
+
+    # local testing
+    app.run(debug=True, host = '0.0.0.0', port = 5000)
+
+    # for Heroku deployment
+    # port = int(os.environ['PORT'])
+    # app.run(port=port, host="0.0.0.0")
