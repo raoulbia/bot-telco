89d56994e14f742e58e6c6572bdf0346004616d6
vagrant <raoulbia@gmail.com>
2019-05-25 11:42:44 +0100

    update

--- Procfile
+++ Procfile
@@ -1,5 +1,5 @@
 # https://medium.com/@gitaumoses4/deploying-a-flask-application-on-heroku-e509e5c76524
 
-web: gunicorn app:app
+web: gunicorn cai_vodafone.app:app
 # web: python app.py
 # web: gunicorn gettingstarted.wsgi --log-file -
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,8 @@
-from cai_vodafone import app
-
-if __name__ == "__main__":
-    app.run()
+"""
+ the wsgi.py file or the alias aren't needed, Gunicorn can be pointed directly at the real module and callable,
+ https://stackoverflow.com/questions/33379287/gunicorn-cant-find-app-when-name-changed-from-application
+"""
+# from cai_vodafone import app
+#
+# if __name__ == "__main__":
+#     app.run()
