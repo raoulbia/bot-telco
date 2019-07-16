da7fc341c18cab176ce6181b2acc3a91d4135c5b
vagrant <raoulbia@gmail.com>
2019-07-16 11:44:08 +0100

    rm wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
- the wsgi.py file or the alias aren't needed, Gunicorn can be pointed directly at the real module and callable,
- https://stackoverflow.com/questions/33379287/gunicorn-cant-find-app-when-name-changed-from-application
-"""
-# from cai_vodafone import app
-#
-# if __name__ == "__main__":
-#     app.run()
